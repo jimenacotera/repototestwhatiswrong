4670789a513381f85cb850df7925de52fb7f22b3
jimenacotera <47250699+jimenacotera@users.noreply.github.com>
2021-11-30 21:16:48 -0500

    updating

--- local.sh
+++ local.sh
@@ -9,6 +9,3 @@ fi
 PYTHONPATH=$(pwd):$PYTHONPATH
 FLASK_APP=API.endpoints flask run --host=$HOST --port=$PORT#!/bin/bash
 
-# run our server locally:
-PYTHONPATH=$(pwd):$PYTHONPATH
-FLASK_APP=endpoints flask run --host=127.0.0.1 --port=8000
